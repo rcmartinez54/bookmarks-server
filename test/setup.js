107af44ddd6ed806a9e56fd7b5add212c76e4de7
Raymond Martinez <rcmartinez1854@gmail.com>
2020-01-20 11:55:24 -0800

    refactored for assignment

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 process.env.NODE_ENV = 'test';
-process.env.API_TOKEN = 'test-auth-token';
+process.env.TZ = 'UTC';
 
 const { expect } = require('chai');
 const supertest = require('supertest');
